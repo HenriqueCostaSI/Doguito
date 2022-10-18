984fe900f7329b396381e5f32da55c42db1f6113
henriqueCostaSI <henrique.mcosta@hotmail.com>
2022-10-18 00:11:19 -0300

    validacao cpf

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -1,3 +1,8 @@
+/**
+ * It validates the input and if it's valid, it removes the class 'input-container--invalido' and the
+ * error message, otherwise it adds the class 'input-container--invalido' and the error message.
+ * @param input - the input element
+ */
 export function valida(input) {
     const tipoDeinput = input.dataset.tipo;
 
@@ -7,33 +12,69 @@ export function valida(input) {
 
     if(input.validity.valid) {
         input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-error').innerHTML = ''
+
     } else {
         input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-error').innerHTML = mostraMensagemDeErro(tipoDeinput, input)
     }
 }
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patterMismatch',
+    'customError'
+];
+
 
 const mensagensDeErro = {
     nome: {
         valueMissing: 'O campo nome não pode estar vazio' 
     },
     email: {
-        valueMissing: 'O campo de email não pode estar vazio' 
+        valueMissing: 'O campo de email não pode estar vazio' ,
         typeMissing: 'O email digitado não é válido.'
-    }
+    },
     senha: {
-        valueMissing: 'O campo senha não pode estar vazio'
+        valueMissing: 'O campo senha não pode estar vazio',
         patternMissing: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um núnero e não deve conter símbolos.'
-    }
+    },
     dataNascimento: {
-        valueMissing: 'O campo de data nascimento não pode estar vazio'
+        valueMissing: 'O campo de data nascimento não pode estar vazio',
         customError: 'Você deve ser maoir de 18 anos para se cadastrar'
+    },
+    cpf: {
+        valueMissing: 'O campo de cpf não pode estar vazio',
+        customError: 'O CPF digitado não é válido'
     }
 
 };
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input)
 };
+
+/**
+ * It loops through the array of error types, and if the input has that error type, it sets the message
+ * to the corresponding error message
+ * @param tipoDeinput - the type of input (text, email, etc)
+ * @param input - the input element
+ * @returns the message.
+ */
+function mostraMensagemDeErro(tipoDeinput, input) {
+    let mensagem = '';
+
+    tiposDeErro.forEach(tipoErro => {
+        if(input.validity[tipoErro]) {
+            mensagem = mensagensDeErro[tipoDeInput][tipoErro];
+        }
+    })
+
+
+    return mensagem;
+}
 
 
 /**
@@ -66,4 +107,46 @@ function maiorQue18(data) {
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
 
     return dataMais18 <= dataAtual;
+}
+
+
+function validaCPF(input) {
+    const cpfFormatado = input.value.replaca(/\D/g, '');
+    
+    let mensagem = '';
+
+    if(!checaCPFRepetido(cpfFormatado)) {
+        mensagem = 'O cpf  digitado não é válido.';
+    }
+
+
+    input.setCustomValidity(mensagem);
+}
+
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    let cpfValido = true;
+
+
+    valoresRepetidos.forEach( valor => {
+        if( valor == cpf) {
+            cpfValido = false;
+        }
+    })
+
+
+
+    return cpfValido;
 }
